f2bb837ecc9bc8f9131f8db0ed86fd712fb1287d
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2023-07-30 21:22:26 +0200

    Work Data&Resume changes

--- src/components/WorkData.js
+++ src/components/WorkData.js
@@ -1,16 +1,17 @@
 export const Work = [
     {
         id:1,
+        name:"MARKETING MANAGER AND DESIGNER",
+        tags:"Ram International",
+        description:"April 2023 - Present", 
+        demo:"https://raminternational.rs/"
+    },
+    {
+        id:2,
         name:"SALES MANAGER AND 2D/3D DESIGNER",
         tags:"Ktitor Company",
         description:"Jun 2021 - October 2022", 
         demo:"https://ktitor.com/"
-    },
-    {
-        id:2,
-        name:"FRONT-END DEVELOPER - INTERNSHIP",
-        tags:"Alenic Solutions",
-        description:"November 2020 - March 2021", 
     },
     {
         id:3,
